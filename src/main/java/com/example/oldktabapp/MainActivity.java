e5e8a091f107185836c5eac448107e20e1431251
zianeabdelhadi <44970377+zianeabdelhadi@users.noreply.github.com>
2022-03-24 00:03:41 +0100

    Page d'inscription

--- src/main/java/com/example/oldktabapp/MainActivity.java
+++ src/main/java/com/example/oldktabapp/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        //Passing each menu ID as a set of Ids because each
+        //menu should be considered as top level destinations.
         //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 //R.id.navigation_annonces, R.id.navigation_compte, R.id.navigation_notifications)
                 //.build();
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        //NavController navController = Navigation.findNavController(this, R.id.navigation_annonces);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         //NavigationUI.setupWithNavController(binding.navView, navController);
     }
